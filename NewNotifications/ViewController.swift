023afe7540e5a26b68b99bb8d657d2fd90e1691d
marlont63 <tavarez.marlon@gmail.com>
2016-10-24 20:53:38 +0200

    Initial Commit

---
+++ NewNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewNotifications
+//
+//  Created by Marlon Raschid Tavarez Parra on 24/10/16.
+//  Copyright © 2016 MyBCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
